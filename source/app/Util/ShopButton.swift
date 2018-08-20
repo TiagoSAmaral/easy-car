dbd14d658cf738ac5f5ce0e09678ee105b07ca03
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-20 13:28:19 -0300

    Limpeza de codigo.

--- source/app/Util/ShopButton.swift
+++ source/app/Util/ShopButton.swift
@@ -11,14 +11,6 @@ import SwiftIconFont
 
 class ShopButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 	override func awakeFromNib() {
 		super.awakeFromNib()
 
