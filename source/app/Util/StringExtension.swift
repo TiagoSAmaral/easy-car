6570e89d6c2f5f71d21b67791f113757f9eb5b06
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-21 01:53:29 -0300

    Ajuste para converter float para currency. Criado enum para controle de Locale

--- source/app/Util/StringExtension.swift
+++ source/app/Util/StringExtension.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-extension String {
+extension Float {
 
-	func currency(value: Float) -> String? {
+	func currency(in locale: CustomLocale) -> String {
 
 		let formatter = NumberFormatter()
+		// Change this to another locale if you want to force a specific locale, otherwise this is redundant as the current locale is the default already
+		formatter.locale = locale.value
 		formatter.numberStyle = .currency
-		if let formattedTipAmount = formatter.string(from: value as NSNumber) {
-			return String(formattedTipAmount)
-		}
 
-		return nil
+		return String(formatter.string(from: self as NSNumber)!)
+
+	}
+}
+
+enum CustomLocale{
+
+	case brazil
+	case unitstate
+
+	var value: Locale {
+		switch self {
+		case .brazil:
+			return Locale(identifier: "pt_BR")
+		case .unitstate:
+			return Locale(identifier: "en_US")
+		}
 	}
 }
