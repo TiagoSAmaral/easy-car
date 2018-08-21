663982ba5922c1f313fecbc36c037506eaa9473d
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-21 01:51:46 -0300

    Criado Stubs para teste de requisição e carregar arquivo de mock.

--- source/app/Util/UIViewControllerExtension.swift
+++ source/app/Util/UIViewControllerExtension.swift
@@ -11,20 +11,33 @@ import  SwiftIconFont
 
 extension UIViewController {
 
-	func createButtonNavigationItem (target: UIViewController, selector: Selector, icon: String) -> UIBarButtonItem {
+	func createButtonNavigationItem (target: UIViewController, selector: Selector?, icon: String?, text: String?, width: Int?) -> UIBarButtonItem {
 
-		let bttn: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 22))
-		bttn.titleLabel?.font = UIFont.icon(from: .fontAwesome, ofSize: 28)
-		bttn.titleLabel?.text = String.fontAwesomeIcon(icon)
+		let widthButton = width != nil ? width! : 32
+		let bttn: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: widthButton, height: 22))
+		var textInternal: String = ""
+
+		if let validIcon = icon {
+			bttn.titleLabel?.font = UIFont.icon(from: .fontAwesome, ofSize: 28)
+			textInternal.append(String.fontAwesomeIcon(validIcon)!)
+		}
+
+		if icon != nil && text != nil {
+			textInternal.append(" ")
+		}
+
+		if let validTexdt = text {
+			textInternal.append(validTexdt)
+		}
+
+		if let validMethod = selector {
+			bttn.addTarget(target.self, action: validMethod, for: .touchUpInside)
+		}
+
+		bttn.titleLabel!.text = textInternal
 		bttn.setTitleColor(.black, for: .normal)
-		bttn.addTarget(target.self, action: selector, for: .touchUpInside)
 		bttn.parseIcon()
 
 		return UIBarButtonItem(customView: bttn)
-	}
-
-	@objc func showAccount () {
-
-		fatalError("Override showAccount function !")
 	}
 }
