c9350373948e876d776e89d10ce56db6188806f1
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-22 18:20:24 -0300

    Refatoramento conforme inplemntaçaõ de Router.

--- source/app/System/AppDelegate.swift
+++ source/app/System/AppDelegate.swift
@@ -16,9 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 
-		let viewController = UIStoryboard(name: "ViewController", bundle: nil).instantiateViewController(withIdentifier: "ViewController")
+		let routerView = RouterView()
+		routerView.presenter = Router(view: routerView)
+		routerView.presenter?.goTo(view: .shop, pushForward: nil)
+
 		self.window = UIWindow(frame: UIScreen.main.bounds)
-		self.window?.rootViewController = viewController
+		self.window?.rootViewController = routerView
 		self.window?.makeKeyAndVisible()
 
 		return true
